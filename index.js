b2775c279c1e5b935026c0609b2cdbcab3650082
gerardmi24 <gerardmistretta@yahoo.com>
2020-10-06 22:35:38 -0400

    Struggled and ultimately had to seek help

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 /***** Deliverable 1 *****/
-const header = document.querySelector("#header")
-console.log("Here's your header:", header)
-
-
+document.addEventListener("DOMContentLoaded", () => {
+    const header = document.querySelector("#header");
+    console.log("Here's your header:", header);
+  
 /***** Deliverable 2 *****/
-
+header.style = "color:red";
 
 /***** Deliverable 3 *****/
-// console.log("PLAYERS array looks like this:", PLAYERS)
+let team = PLAYERS[0]
 
+    
+        function renderPlayer(playerArray) {
+            for (const player of playerArray) {
+            const playerDiv = document.createElement('div')
+            playerDiv.classList.add("player")
+            playerDiv.dataset.number = `${player.number}`
+            playerDiv.innerHTML = `
+                <h3>${player.name} (<em>${player.nickname}</em>)</h3>
+                <img src="${player.photo}" alt="${player.name}">
+        `
+        
+            const playerContainer = document.querySelector(".player-container")
+            playerContainer.append(playerDiv)
+        }
+    }
+    renderPlayer(PLAYERS);
 
 /***** Deliverable 4 *****/
+document.querySelector("div[data-number='7']").remove();
+});
